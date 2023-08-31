0a211716ff58e83fb7a06aea0b0c3fe9afe45f57
Thor <102772895+thorcamara@users.noreply.github.com>
2023-08-31 16:21:04 -0300

    Commit .

---
+++ database.rb
@@ -0,0 +1,30 @@
+require 'pstore'
+
+class Database
+  def initialize
+    @store = PStore.new("notes.store")
+  end
+
+  def store(note)
+    @store.transaction do
+      @store[note.id.to_sym] = note
+    end
+  end
+
+  def get_all()
+    notes = []
+    @store.transaction do
+      @store.roots.each do |id|
+        notes.push(@store[id])
+      end
+    end
+    notes
+  end
+
+  def delete(note)
+    @store.transaction do
+      @store.delete(note.id.to_sym)
+    end
+  end
+
+end
